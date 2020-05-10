0c94f581e5365c74e69af9b99c64952b1ad92a73
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-05-10 16:25:12 +0300

    Переименовал переменные, чтобы было удобнее

--- js/next-action.js
+++ js/next-action.js
@@ -1,43 +1,65 @@
+// Ждем, пока сайт полностью загрузится
 window.addEventListener('load', function() {
+    // Добавляем функцию, которая срабатывает при событии onclick (при клике)
     document.getElementById('next').onclick = function(event) {
+        // Отключаем дефолтное поведение ссылки,
+        // чтобы браузер не поменял страницу
         event.preventDefault();
         
-        const EXERCISES = 18;
+        // Кол-во упражнений
+        const LAST_EXERCISE = 18;
+        // Счетчики выполненых упражнений из сета
+        const SET_EX_COUNT_1 = 0;
+        const SET_EX_COUNT_2 = 1;
+        // Кол-во упражнений в сете
         const SET_END = 6;
 
-        let nextPage;
+        // Количество выполненых упражнений
+        let exCount = Number(get('exCount'));
+        // Номер упражнения из текущего сета
+        let setExercise = Number(get('setExercise'));
+        // Полный адрес сайта, разбитый на части по знаку /
         let url = window.location.href.split('/');
-        let currentPage = url.slice(-1)[0];
+        // Адрес сайта, от которого отрезали последний кусочек
         let baseUrl = url.slice(0, -1).join('/');
-        let exercise = Number(get('exercise'));
-        let exerciseType = get('exerciseType');
+        // Объявляем переменную, чтобы воспользоваться ей позже 
+        let nextPage;
         
-        if (currentPage !== 'rest.html') {
-            window.location.href = baseUrl + '/rest.html';
-        }
-        
-        if (exercise === EXERCISES) {
+        // Если выполнили все упражнения, то
+        if (exCount === LAST_EXERCISE) {
+            // следующая страница — заминка
             nextPage = 'core.html';
         } else {
-            if (exercise % SET_END === 0) {
-                set('set', Number(get('set')) + 1);
-                set('aNum', 1);
-                set('bNum', 1);
-            }
-            if (exerciseType === 'a') {
-                set('exerciseType', 'b');
-                set('aNum', Number(get('aNum')) + 1); 
+            // Если выполнили все упражнения в сете
+            if (exCount % SET_END === 0) {
+                // то увеличиваем кол-во выполненых сетов
+                set('setCount', Number(get('setCount')) + 1);
+                // и сбрасываем счетчики упражнений в сетах
+                set('setExCount1', 1);
+                set('setExCount2', 1);
             } else {
-                set('exerciseType', 'a');
-                set('bNum', Number(get('bNum')) + 1);
+                // Увеличиваем счетчик выполненых упражнений в сете
+                // и меняем текущий сет на противоположный
+                if (setExercise === SET_EX_COUNT_1) {
+                    set('setExCount1', Number(get('setExCount1')) + 1); 
+                    set('setExercise', SET_EX_COUNT_2);
+                } else {
+                    set('setExCount2', Number(get('setExCount2')) + 1);
+                    set('setExercise', SET_EX_COUNT_1);
+                }
             }
-            nextPage = get('set') + get('exerciseType') + '.html';
-            set('exercise', exercise + 1);
+            // Увеличиваем общий счетчик выполненых упражнений
+            set('exCount', exCount + 1);
+            // устанавливаем адрес следующей страницы
+            nextPage = 'exercise.html';
         }
 
+        // переносим пользователя на следующую страницу
         window.location.href = baseUrl + `/${nextPage}`;
     }
 });
+
+// Обе функции ниже написаны, чтобы сэкономить место на экране
 
 function get(item) {
     return localStorage.getItem(item);
